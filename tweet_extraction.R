fee498808b1e506a6a13ca4b48c98653e7ea26ca
MatteoLarrode <matteo.larrode@orange.fr>
2023-02-06 01:30:17 -0500

    api auth

--- tweet_extraction.R
+++ tweet_extraction.R
@@ -1,1 +1,14 @@
 # Extract tweets related to AI 
+
+# load packages ------------------------
+library(tidyverse)
+library(rtweet)
+
+#authentification to twitter API
+auth_as("matteo_twitter")
+
+
+# data collection ------------------------
+
+
+
